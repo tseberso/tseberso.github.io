04ddb4cc147d45437b15778b63544bff0dd67034
Troy Seberson <troy@Troys-MacBook-Pro.local>
2025-09-14 17:54:46 -0500

    add properties

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -5,7 +5,7 @@ import { useLocation } from "react-router-dom";
 export default function ScrollToTop() {
   const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "instant" as ScrollBehavior });
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" as ScrollBehavior });
   }, [pathname]);
   return null;
 }
